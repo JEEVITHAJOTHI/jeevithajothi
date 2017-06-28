099537910efef73f7987bef89c7c2b0a4860a7bf
JEEVITHAJOTHI <jeevithajothi97@gmail.com>
2017-06-28 15:54:24 +0530

    Add files via upload

---
+++ Natural.java
@@ -0,0 +1,18 @@
+package guvi;
+import java.util.Scanner;
+public class Natural {
+
+	public static void main(String[] args)
+	{
+		Scanner s=new Scanner(System.in);
+		int m=0;
+		int x=s.nextInt();
+		for(int i=1;i<=x;i++)
+		{
+			m=m+i;
+		}
+		System.out.println("The sum of "+x+" numbers is "+m);
+		
+	}
+
+}
