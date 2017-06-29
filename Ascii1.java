5f4e71c3a044b647986959bd6cf20d551104cd5d
JEEVITHAJOTHI <jeevithajothi97@gmail.com>
2017-06-29 14:32:55 +0530

    Add files via upload

---
+++ Ascii1.java
@@ -0,0 +1,12 @@
+package guvi1;
+
+public class Ascii1 {
+
+	public static void main(String[] args)
+	{
+         for(int i=0;i<26;i++)
+         {
+        	 System.out.println((char)('A'+i)+"====="+('A'+i));
+         }
+}
+}
