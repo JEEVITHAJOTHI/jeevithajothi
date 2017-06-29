8d0191e310a8a3f05c4106563782ec6a41cebb72
JEEVITHAJOTHI <jeevithajothi97@gmail.com>
2017-06-29 15:05:10 +0530

    Add files via upload

---
+++ Less.java
@@ -0,0 +1,17 @@
+package guvi1;
+import java.util.Scanner;
+public class Less {
+
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		int a=s.nextInt();
+		int b=0;
+		for(int i=0;i<=a;i++)
+		{
+			b=b+i;
+		}
+s.close();
+		System.out.println(b);
+	}
+
+}
