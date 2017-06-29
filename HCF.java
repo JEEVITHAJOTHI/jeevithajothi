3505ddb8cf801d8daf86e5974d0dda4d22625d97
JEEVITHAJOTHI <jeevithajothi97@gmail.com>
2017-06-29 14:07:01 +0530

    Add files via upload

---
+++ HCF.java
@@ -0,0 +1,25 @@
+package guvi1;
+import java.util.Scanner;
+public class HCF {
+
+	        public static void main(String args[])
+	        {
+	            Scanner sc = new Scanner(System.in);
+	            System.out.print("Enter the First no : ");
+	            int n1=sc.nextInt();
+	            System.out.print("Enter the Second no : ");
+	            int n2=sc.nextInt();
+	            int r;
+	             
+	            while(n2 != 0)
+	            {
+	                r = n1 % n2;
+	                n1 = n2;
+	                n2 = r;
+	            }
+	            System.out.print("HCF = "+n1);
+	            
+	        }
+	    
+	    }
+
