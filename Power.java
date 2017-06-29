97c7df6d8b677a8e4e3aeb05603713b526e64408
JEEVITHAJOTHI <jeevithajothi97@gmail.com>
2017-06-29 09:33:52 +0530

    Add files via upload

---
+++ Power.java
@@ -0,0 +1,14 @@
+package guvi;
+import java.util.Scanner;
+public class Power {
+
+	public static void main(String[] args)
+	{
+		Scanner s=new Scanner(System.in);
+		int a=s.nextInt();
+		int b=s.nextInt();
+		System.out.println(Math.pow(a,b));
+
+	}
+
+}
