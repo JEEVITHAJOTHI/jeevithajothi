2aeca79607619fbb86017b82f1401e022b6f2596
JEEVITHAJOTHI <jeevithajothi97@gmail.com>
2017-06-29 14:59:49 +0530

    Add files via upload

---
+++ Q.java
@@ -0,0 +1,16 @@
+package guvi1;
+import java.util.Scanner;
+public class Q {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner s=new Scanner(System.in);
+		String c=s.next();
+		if(c.equals("Q"))
+		{
+			s.close();
+		}
+
+	}
+
+}
