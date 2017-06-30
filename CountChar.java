3cd128bbc25eedff642afa8923e45f10ec5e4cd0
JEEVITHAJOTHI <jeevithajothi97@gmail.com>
2017-06-30 12:07:03 +0530

    Add files via upload

---
+++ CountChar.java
@@ -0,0 +1,15 @@
+package guvi1;
+import java.util.Scanner;
+public class CountChar {
+
+	public static void main(String[] args) {
+		
+		Scanner s=new Scanner(System.in);
+		String a=s.next();
+		int b=a.length();
+		System.out.println(b);
+		
+
+	}
+
+}
