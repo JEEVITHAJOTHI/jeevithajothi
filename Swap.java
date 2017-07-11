5e38d0dc5f4270eb3a6887610e7615e2e00a3885
JEEVITHAJOTHI <jeevithajothi97@gmail.com>
2017-07-11 17:13:26 +0530

    Add files via upload

---
+++ Swap.java
@@ -0,0 +1,17 @@
+package Logic;
+import java.util.Scanner;
+public class Swap {
+
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		int a=9;
+		int b=56;
+		int t=0;
+		t=a;
+		a=b;
+		b=t;
+		System.out.println(a+" "+b);
+
+	}
+
+}
